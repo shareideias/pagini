aaa560ae3752ac56e699842d9d8917436363a7ef
Juliana Nery Schwartz <juliana.schwartz@hotmail.com>
2020-09-21 18:54:52 -0300

    Criação de rotas das telas estáticas
    Criação do componente-navbar e uso do nome das rotas para os hrefs

--- resources/views/administracao.blade.php
+++ resources/views/administracao.blade.php
@@ -19,35 +19,10 @@
 
       <!--cabeçalho-->
       <header>
-        <nav class="nav-wrapper transparent">
-          <div class="container">
-            <a href="/" class="brand-logo"><img class="imagem_logo" alt="Logo da Share" src="../img/globo.png"></a>
-            <a href="" class="sidenav-trigger" data-target = "mobile-menu">
-              <i class="material-icons">menu</i>
-            </a>
-
-            <!--menu desktop-->
-            <ul class="right hide-on-med-and-down">
-              <li><a class="link_menu" href="/">Notícias</a></li>
-              <li><a class="link_menu" href="modelo.html">Institucional</a></li>
-              <li><a class="link_menu" href="/cursos">Cursos</a></li>
-              <li><a class="link_menu" href="#">Inscrições</a></li>
-              <li><a class="link_menu" href="/faq">FAQ</a></li>
-              <li><a class="link_menu" href="login.html">Login</a></li>
-            </ul>
-            <!--menu mobile-->
-            <ul class="sidenav lighten-2" id="mobile-menu">
-              <li><a href="#panel-heading">Notícias</a></li>
-              <li><a href="modelo.html">Institucional</a></li>
-              <li><a href="/cursos">Cursos</a></li>
-              <li><a href="#">Inscrições</a></li>
-              <li><a href="/faq">FAQ</a></li>
-              <li><a href="login.html">Login</a></li>
-            </ul>
-          </div>
-        </nav>
+        @include('componente_navbar')
       </header>
-
+      <!--fim cabeçalho-->
+      
       <main>
 
         <h2>Administração</h2>
--- resources/views/cursos.blade.php
+++ resources/views/cursos.blade.php
@@ -37,33 +37,7 @@
 
       <!--cabeçalho-->
       <header>
-        <nav class="nav-wrapper transparent">
-          <div class="container">
-            <a href="/" class="brand-logo"><img class="imagem_logo" alt="Logo da Share" src="../img/globo.png"></a>
-            <a href="" class="sidenav-trigger" data-target = "mobile-menu">
-              <i class="material-icons">menu</i>
-            </a>
-             
-             <!--menu desktop-->
-            <ul class="right hide-on-med-and-down">
-              <li><a class="link_menu" href="#panel-heading">Notícias</a></li>
-              <li><a class="link_menu" href="#">Institucional</a></li>
-              <li><a class="link_menu" href="/cursos">Cursos</a></li>
-              <li><a class="link_menu" href="#">Inscrições</a></li>
-              <li><a class="link_menu" href="/faq">FAQ</a></li>
-              <li><a class="link_menu" href="login.html">Login</a></li>
-            </ul>
-            <!--menu mobile-->
-            <ul class="sidenav lighten-2" id="mobile-menu">
-              <li><a href="#panel-heading">Notícias</a></li>
-              <li><a href="modelo.html">Institucional</a></li>
-              <li><a href="/cursos">Cursos</a></li>
-              <li><a href="#">Inscrições</a></li>
-              <li><a href="/faq">FAQ</a></li>
-              <li><a href="login.html">Login</a></li>
-            </ul> 
-          </div>
-        </nav>    
+        @include('componente_navbar')
       </header>
       <!--fim cabeçalho-->
 
